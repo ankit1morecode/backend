39528c8b4cb21d4ce5708b0b57756498fd4f6793
ankit1morecode <ankitsafeid@gmail.com>
2026-06-03 16:48:38 +0530

    basic-todo

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 let app = require("./src/app");
 
+app.set("view engine", "ejs");
+
+
 app.listen(3000,()=>{
     console.log("app is listening on port 3000");
 })
--- src/app.js
+++ src/app.js
@@ -1,14 +1,31 @@
 const express = require("express");
-
 let app = express();
+app.use(express.urlencoded({ extended: true }));
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 let arr = [];
-
-app.get("/",(req,res)=>{
+app.post("/notes",(req,res)=>{
     let data = req.body;
-    arr.push(data);
-    res.render("index.js",{arr});
+    if(data.title != "" || data.description!=""){
+        let obj = {
+        title : data.title,
+        description : data.description
+    }
+        arr.push(obj);
+        res.render("index.ejs",{arr});
+    }else{
+        res.send("Enter title or desctiprion to add the task");
+    }
 })
 
+app.delete("/delete",(req,res)=>{
+    arr.pop();
+    res.render("index.ejs",{arr});
+})
+
+app.get("/",(req,res)=>{
+    res.render("index.ejs",{arr});
+})
 
 module.exports = app;
